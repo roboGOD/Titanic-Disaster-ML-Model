557615f2634624da6c588b8b2cb3de841f037aa4
Ishwer Kumar <ishwerkumar666@gmail.com>
2019-01-10 23:17:08 +0530

    Vizualizing Dataset

--- model.py
+++ model.py
@@ -1,4 +1,115 @@
 import pandas as pd 
+from matplotlib import pyplot as plt
+from matplotlib import style
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
 
+
+#######################################################################
+### Loading the Dataset
 dataset = pd.read_csv('dataset/train.csv')
-print dataset.head(3)
+
+### Given Columns
+''' 
+PassengerId
+Pclass
+Name
+Sex
+Age
+SibSp
+Parch
+Ticket
+Fare
+Cabin
+Embarked
+Survived
+'''
+
+#######################################################################
+### Preprocess the data
+
+# Drop the Cabin Column
+nd_dataset = dataset.drop(['Cabin'], axis=1)
+
+# Assign mean to the Age column NA values
+nd_dataset['Age'] = nd_dataset['Age'].fillna(nd_dataset['Age'].mean())
+
+# Drop all the NA values
+nd_dataset = nd_dataset.dropna()
+
+
+# Convert Strings to numbers
+l = []
+for i in nd_dataset['Sex']:
+	if i == "male":
+		l.append(0.)
+	elif i == "female":
+		l.append(1.)
+	else:
+		print "Error", i
+nd_dataset['Sex_n'] = pd.Series(l)
+
+l = []
+for i in nd_dataset['Embarked']:
+	if i == "S":
+		l.append(0.)
+	elif i == "C":
+		l.append(1.)
+	elif i == "Q":
+		l.append(2.)
+	else:
+		print "Error", i
+nd_dataset['Embarked_n'] = pd.Series(l)
+
+
+# Modify the actual dataset
+dataset = nd_dataset
+
+
+#######################################################################
+### Visualizing Data
+surv_data = dataset[dataset['Survived'] == 1]
+died_data = dataset[dataset['Survived'] == 0]
+
+style.use('fivethirtyeight')
+#plt.scatter(surv_data['PassengerId'].values, surv_data['Age'].values, label='Survived', color='g')
+#plt.scatter(died_data['PassengerId'].values, died_data['Age'].values, label='Died', color='r')
+
+#plt.hist(surv_data['Fare'], label='Survived', color='g')
+#plt.hist(died_data['Fare'], label='Died', color='r')
+
+plt.subplot(1,2,1)
+plt.hist(surv_data['Age'], label='Survived', color='g')
+plt.xlabel('Age', size=10)
+plt.ylabel('Frequency', size=10)
+plt.xticks(size=8)
+plt.yticks(size=8)
+plt.title('Survived', size=10)
+
+plt.subplot(1,2,2)
+plt.hist(died_data['Age'], label='Died', color='r')
+plt.xlabel('Age', size=10)
+plt.ylabel('Frequency', size=10)
+plt.xticks(size=8)
+plt.yticks(size=8)
+plt.title('Died', size=10)
+
+plt.show()
+
+
+'''
+#######################################################################
+### Train-test Split
+
+feature_list = ['Fare', 'Pclass', 'Sex_n', 'Age', 'Embarked_n', 'SibSp', 'Parch']
+labels = dataset['Survived'].values
+
+X_train, X_val, y_train, y_val = \
+		train_test_split(dataset[feature_list].values, labels, test_size=0.2, random_state=42)
+
+
+### Classification Model
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+print clf.score(X_val, y_val)
+'''
