adb29e87d2997aa6f1961b9431b6e36290429f78
Ishwer Kumar <ishwerkumar666@gmail.com>
2019-01-18 19:17:41 +0530

    Trying Random Forest Classifier

--- model.py
+++ model.py
@@ -43,6 +43,8 @@ X_train, X_val, y_train, y_val = \
 
 ### Classification Model
 
+
+######################## Decision Tree ###############################
 ### Finding best value for min_samples_split as 14 
 # samp_vals = [x for x in range(2,100,3)]
 # accuracies = []
@@ -69,7 +71,7 @@ X_train, X_val, y_train, y_val = \
 # plt.ylabel('Accuracy')
 # plt.show()
 
-
+######################## AdaBoost ###############################
 ### Finding best value for n_estimators as 260
 # samp_vals = [x for x in range(150,400,10)]
 # accuracies = []
@@ -83,7 +85,50 @@ X_train, X_val, y_train, y_val = \
 # plt.ylabel('Accuracy')
 # plt.show()
 
-clf = AdaBoostClassifier(n_estimators = 260, random_state=42)
+######################## Random Forest ###############################
+### Finding best value for min_samples_split as 8 
+# samp_vals = [x for x in range(2,20,1)]
+# accuracies = []
+# for i in samp_vals:
+# 	clf = RandomForestClassifier(min_samples_split=i, random_state=42)
+# 	clf.fit(X_train, y_train)
+# 	accuracies.append(clf.score(X_val, y_val))
+
+# plt.plot(samp_vals, accuracies, '-b')
+# plt.xlabel('Min. Samples Split')
+# plt.ylabel('Accuracy')
+# plt.show()
+
+### Finding best value for max_depth as 21
+# samp_vals = [x for x in range(2,100,3)]
+# accuracies = []
+# for i in samp_vals:
+# 	clf = RandomForestClassifier(min_samples_split=8, max_depth=i, random_state=42)
+# 	clf.fit(X_train, y_train)
+# 	accuracies.append(clf.score(X_val, y_val))
+
+# plt.plot(samp_vals, accuracies, '-b')
+# plt.xlabel('Max Depth')
+# plt.ylabel('Accuracy')
+# plt.show()
+
+### Finding best value for n_estimators as 5
+# samp_vals = [x for x in range(2,30,1)]
+# accuracies = []
+# for i in samp_vals:
+# 	clf = RandomForestClassifier(n_estimators=i, min_samples_split=8, max_depth=21,  random_state=42)
+# 	clf.fit(X_train, y_train)
+# 	accuracies.append(clf.score(X_val, y_val))
+
+# plt.plot(samp_vals, accuracies, '-b')
+# plt.xlabel('n_estimators')
+# plt.ylabel('Accuracy')
+# plt.show()
+
+
+
+
+clf = RandomForestClassifier(n_estimators = 5, min_samples_split=8, max_depth=21,  random_state=42)
 clf_ = clf.fit(X_train, y_train)
 
 
@@ -98,25 +143,25 @@ print "Validation Set Score:", clf.score(X_val, y_val)
 
 ### Cross Validation of Model
 
-# test_classifier(clf, features, labels, folds=100)
+test_classifier(clf, features, labels, folds=100)
 
 
 ############################################################################
 ### Make predictions
 
-test_set = pd.read_csv('dataset/test.csv')
-test_set = preprocess(test_set)
-test_features = split_features_labels(test_set, feature_list)
+# test_set = pd.read_csv('dataset/test.csv')
+# test_set = preprocess(test_set)
+# test_features = split_features_labels(test_set, feature_list)
 
-clf.fit(features, labels)
-predictions = clf.predict(test_features)
+# clf.fit(features, labels)
+# predictions = clf.predict(test_features)
 
-output_df = pd.DataFrame()
-output_df['PassengerId'] = test_set['PassengerId']
-output_df['Survived'] = pd.Series(predictions)
+# output_df = pd.DataFrame()
+# output_df['PassengerId'] = test_set['PassengerId']
+# output_df['Survived'] = pd.Series(predictions)
 
-with open('dataset/Ada_out.csv', 'w') as output_csv:
-	output_df.to_csv(output_csv, sep=',', index=False)
+# with open('dataset/Ada_out.csv', 'w') as output_csv:
+# 	output_df.to_csv(output_csv, sep=',', index=False)
 
 
 
