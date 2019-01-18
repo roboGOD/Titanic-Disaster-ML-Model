176398ea8e36d23381329553387556031384167c
Ishwer Kumar <ishwerkumar666@gmail.com>
2019-01-19 01:41:15 +0530

    Trying KNN Classifier

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier as DTClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 
 from preprocess import preprocess, split_features_labels
 from visualize import visualize
@@ -43,92 +45,23 @@ X_train, X_val, y_train, y_val = \
 
 ### Classification Model
 
-
-######################## Decision Tree ###############################
-### Finding best value for min_samples_split as 14 
-# samp_vals = [x for x in range(2,100,3)]
-# accuracies = []
-# for i in samp_vals:
-# 	clf = DTClassifier(min_samples_split=i, random_state=42)
-# 	clf.fit(X_train, y_train)
-# 	accuracies.append(clf.score(X_val, y_val))
-
-# plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('Min. Samples Split')
-# plt.ylabel('Accuracy')
-# plt.show()
-
-### Finding best value for max_depth as 11
-# samp_vals = [x for x in range(2,100,3)]
-# accuracies = []
-# for i in samp_vals:
-# 	clf = DTClassifier(min_samples_split=14, max_depth=i, random_state=42)
-# 	clf.fit(X_train, y_train)
-# 	accuracies.append(clf.score(X_val, y_val))
-
-# plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('Max Depth')
-# plt.ylabel('Accuracy')
-# plt.show()
-
-######################## AdaBoost ###############################
-### Finding best value for n_estimators as 260
-# samp_vals = [x for x in range(150,400,10)]
-# accuracies = []
-# for i in samp_vals:
-# 	clf = AdaBoostClassifier(n_estimators=i, random_state=42)
-# 	clf.fit(X_train, y_train)
-# 	accuracies.append(clf.score(X_val, y_val))
-
-# plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('n_estimators')
-# plt.ylabel('Accuracy')
-# plt.show()
-
-######################## Random Forest ###############################
-### Finding best value for min_samples_split as 8 
-# samp_vals = [x for x in range(2,20,1)]
-# accuracies = []
-# for i in samp_vals:
-# 	clf = RandomForestClassifier(min_samples_split=i, random_state=42)
-# 	clf.fit(X_train, y_train)
-# 	accuracies.append(clf.score(X_val, y_val))
-
-# plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('Min. Samples Split')
-# plt.ylabel('Accuracy')
-# plt.show()
-
-### Finding best value for max_depth as 21
-# samp_vals = [x for x in range(2,100,3)]
-# accuracies = []
-# for i in samp_vals:
-# 	clf = RandomForestClassifier(min_samples_split=8, max_depth=i, random_state=42)
-# 	clf.fit(X_train, y_train)
-# 	accuracies.append(clf.score(X_val, y_val))
-
-# plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('Max Depth')
-# plt.ylabel('Accuracy')
-# plt.show()
-
-### Finding best value for n_estimators as 5
+######################## KNN ###############################
+### Finding best value for n_neighbors as 18 
 # samp_vals = [x for x in range(2,30,1)]
 # accuracies = []
 # for i in samp_vals:
-# 	clf = RandomForestClassifier(n_estimators=i, min_samples_split=8, max_depth=21,  random_state=42)
+# 	clf = KNeighborsClassifier(n_neighbors=i)
 # 	clf.fit(X_train, y_train)
 # 	accuracies.append(clf.score(X_val, y_val))
 
 # plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('n_estimators')
+# plt.xlabel('n_neighbors')
 # plt.ylabel('Accuracy')
 # plt.show()
 
 
-
-
-clf = RandomForestClassifier(n_estimators = 5, min_samples_split=8, max_depth=21,  random_state=42)
+# clf = RandomForestClassifier(n_estimators = 5, min_samples_split=8, max_depth=21,  random_state=42)
+clf = KNeighborsClassifier(n_neighbors = 18, weights = 'distance', p=1)
 clf_ = clf.fit(X_train, y_train)
 
 
@@ -149,21 +82,21 @@ test_classifier(clf, features, labels, folds=100)
 ############################################################################
 ### Make predictions
 
-# test_set = pd.read_csv('dataset/test.csv')
-# test_set = preprocess(test_set)
-# test_features = split_features_labels(test_set, feature_list)
+test_set = pd.read_csv('dataset/test.csv')
+test_set = preprocess(test_set)
+test_features = split_features_labels(test_set, feature_list)
 
-# clf.fit(features, labels)
-# predictions = clf.predict(test_features)
+clf.fit(features, labels)
+predictions = clf.predict(test_features)
 
-# output_df = pd.DataFrame()
-# output_df['PassengerId'] = test_set['PassengerId']
-# output_df['Survived'] = pd.Series(predictions)
+output_df = pd.DataFrame()
+output_df['PassengerId'] = test_set['PassengerId']
+output_df['Survived'] = pd.Series(predictions)
 
-# with open('dataset/Ada_out.csv', 'w') as output_csv:
-# 	output_df.to_csv(output_csv, sep=',', index=False)
+with open('dataset/KNN_out.csv', 'w') as output_csv:
+	output_df.to_csv(output_csv, sep=',', index=False)
 
-
+print "Done!"
 
 
 
