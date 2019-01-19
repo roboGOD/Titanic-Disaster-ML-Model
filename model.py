0591ac7bd138f4f48a090c1867cc2fcf022a4416
Ishwer Kumar <ishwerkumar666@gmail.com>
2019-01-19 23:11:05 +0530

    Tuning Logistic Regression Parameters

--- model.py
+++ model.py
@@ -47,12 +47,12 @@ X_train, X_val, y_train, y_val = \
 
 base_est = DTClassifier(min_samples_split=14, max_depth=11, random_state=42)
 
-######################## SVC ###############################
-### Finding best value for C as 6.5
-# samp_vals = [6+x*0.1 for x in range(1,40)]
+######################## LogisticRegression ###############################
+### Finding best value for C as .045
+# samp_vals = [x*0.001 for x in range(1,100)]
 # accuracies = []
 # for i in samp_vals:
-# 	clf = SVC(C=i, random_state=42)
+# 	clf = LogisticRegression(C=i, random_state=42)
 # 	clf.fit(X_train, y_train)
 # 	accuracies.append(clf.score(X_val, y_val))
 
@@ -61,27 +61,41 @@ base_est = DTClassifier(min_samples_split=14, max_depth=11, random_state=42)
 # plt.ylabel('Accuracy')
 # plt.show()
 
-### Finding best value for gamma as 0.0023
-# samp_vals = [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0, 100.0]
-# samp_vals = [x*0.0001 for x in range(1,100)]
+### Finding best value for solver as lbfgs
+# samp_vals = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
 # accuracies = []
 # for i in samp_vals:
-# 	clf = SVC(C=6.5, gamma=i, random_state=42)
+# 	clf = LogisticRegression(C=0.045, solver=i, random_state=42)
 # 	clf.fit(X_train, y_train)
 # 	accuracies.append(clf.score(X_val, y_val))
 
-# plt.plot(samp_vals, accuracies, '-b')
-# plt.xlabel('gamma')
+# plt.bar(samp_vals, accuracies)
+# plt.xlabel('solver')
+# plt.ylabel('Accuracy')
+# plt.show()
+
+### Finding best value for multi_class as ovr
+# samp_vals = ['ovr', 'multinomial']
+# accuracies = []
+# for i in samp_vals:
+# 	clf = LogisticRegression(C=0.045, solver='lbfgs', multi_class=i, random_state=42)
+# 	clf.fit(X_train, y_train)
+# 	accuracies.append(clf.score(X_val, y_val))
+
+# plt.bar(samp_vals, accuracies)
+# plt.xlabel('multi_class')
 # plt.ylabel('Accuracy')
 # plt.show()
 
 
 
+# clf = DTClassifier(min_samples_split=14, max_depth=11, random_state=42)
 # clf = RandomForestClassifier(n_estimators = 5, min_samples_split=8, max_depth=21,  random_state=42)
 # clf = KNeighborsClassifier(n_neighbors = 18, weights = 'distance', p=1)
 # clf = AdaBoostClassifier(base_estimator=base_est, n_estimators=20, random_state=42)
 # clf = GaussianNB()
-clf = SVC(C=6.5, gamma=0.0023, random_state=42)
+# clf = SVC(C=6.5, gamma=0.0023, random_state=42)
+clf = LogisticRegression(C=0.045, solver='lbfgs', random_state=42)
 clf_ = clf.fit(X_train, y_train)
 
 
@@ -102,21 +116,21 @@ test_classifier(clf, features, labels, folds=100)
 ############################################################################
 ### Make predictions
 
-test_set = pd.read_csv('dataset/test.csv')
-test_set = preprocess(test_set)
-test_features = split_features_labels(test_set, feature_list)
+# test_set = pd.read_csv('dataset/test.csv')
+# test_set = preprocess(test_set)
+# test_features = split_features_labels(test_set, feature_list)
 
-clf.fit(features, labels)
-predictions = clf.predict(test_features)
+# clf.fit(features, labels)
+# predictions = clf.predict(test_features)
 
-output_df = pd.DataFrame()
-output_df['PassengerId'] = test_set['PassengerId']
-output_df['Survived'] = pd.Series(predictions)
+# output_df = pd.DataFrame()
+# output_df['PassengerId'] = test_set['PassengerId']
+# output_df['Survived'] = pd.Series(predictions)
 
-with open('dataset/_out.csv', 'w') as output_csv:
-	output_df.to_csv(output_csv, sep=',', index=False)
+# with open('dataset/_out.csv', 'w') as output_csv:
+# 	output_df.to_csv(output_csv, sep=',', index=False)
 
-print "Done!"
+# print "Done!"
 
 
 
